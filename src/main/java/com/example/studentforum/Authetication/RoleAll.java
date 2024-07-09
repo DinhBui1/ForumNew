0c131c3ba10d025e4019f247897be4a0a0f37ad4
DinhBui1 <115530102+DinhBui1@users.noreply.github.com>
2024-07-09 21:46:14 +0700

    .

--- src/main/java/com/example/studentforum/Authetication/RoleAll.java
+++ src/main/java/com/example/studentforum/Authetication/RoleAll.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class RoleAll {
     public static List<GrantedAuthority> getRoleAll() {
         List<GrantedAuthority> role = new ArrayList<>();
+        role.add(new SimpleGrantedAuthority("ADMIN"));
         role.add(new SimpleGrantedAuthority("USER"));
         role.add(new SimpleGrantedAuthority("ROLE_ANONYMOUS"));
         return role;
